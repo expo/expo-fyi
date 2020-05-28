a691f0d27fc325cc83766dd24aed280286743612
Brent Vatne <brentvatne@gmail.com>
2020-05-28 07:55:16 -0700

    Fix a couple lil bugs

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
 module.exports = (request, response) => {
-  const { url } = request
-  const urlParts = url.split('/')
+  const { url } = request;
+  const urlParts = url.split("/");
 
-  if (url === '/') {
+  if (url === "/") {
     response.writeHead(302, {
-      Location: `https://github.com/expo/fyi`
-    })
+      Location: `https://github.com/expo/fyi`,
+    });
 
-    response.end()
-    return
+    response.end();
+    return;
   }
 
   if (urlParts.length > 2) {
     // who cares? try anyways
   }
 
-
-  const code = urlParts[1];
+  const code = removeWeirdWindowsThing(removeQueryParam(urlParts[1]));
 
   response.writeHead(302, {
-    Location: `https://github.com/expo/fyi/blob/master/${code}.md`
-  })
+    Location: `https://github.com/expo/fyi/blob/master/${code}.md`,
+  });
 
-  response.end()
+  response.end();
+};
+
+function removeQueryParam(text) {
+  return text.split("?")[0];
+}
+
+function removeWeirdWindowsThing(text) {
+  return text.replace("%E2%80%8B", "");
 }
