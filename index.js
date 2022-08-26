143bcb94b37c2511ea3fcc8bd9211ac3c52e1715
Brent Vatne <brentvatne@gmail.com>
2022-08-26 11:00:00 -0700

    Add redirect

--- index.js
+++ index.js
@@ -16,13 +16,23 @@ module.exports = (request, response) => {
   }
 
   const code = removeWeirdWindowsThing(removeQueryParam(urlParts[1]));
+  const name = maybeRedirect(removeMdExtension(code));
 
   response.writeHead(302, {
-    Location: `https://github.com/expo/fyi/blob/main/${removeMdExtension(code)}.md`,
+    Location: `https://github.com/expo/fyi/blob/main/${name}.md`,
   });
 
   response.end();
 };
+
+// Remap names if needed
+function maybeRedirect(name) {
+  if (name === 'LegacyNotifications-to-ExpoNotifications') {
+    return 'legacy-notifications-to-expo-notifications';
+  }
+
+  return name;
+}
 
 function removeQueryParam(text) {
   return text.split("?")[0];
